ed95da70ab5a1ab7223e976d0c99760ff1a66bd5
vector_ma <msx-2020@sjtu.edu.cn>
2026-01-28 19:34:19 +0800

    为TiDAR做的修改

--- trainer/trainer_utils.py
+++ trainer/trainer_utils.py
@@ -13,7 +13,7 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import Sampler
 from transformers import AutoTokenizer
-from model.model_minimind import MiniMindForCausalLM
+from minimind.model.model_minimind import MiniMindForCausalLM
 
 def get_model_params(model, config):
     total = sum(p.numel() for p in model.parameters()) / 1e6
